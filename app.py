c9270716083c4224d620047d16f97871598d2497
Neetha Reddy <44060481+purplesmurf45@users.noreply.github.com>
2022-03-16 03:27:37 +0530

    Fixed choropleth legend

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from dash.dependencies import Input, Output, State
 import cufflinks as cf
 import plotly.graph_objects as go
 import networkx as nx
+from sklearn import preprocessing
 
 # Initialize app
 
@@ -29,96 +30,18 @@ APP_PATH = str(pathlib.Path(__file__).parent.resolve())
 
 df_od = pd.read_csv('https://raw.githubusercontent.com/purplesmurf45/Migration-visualisation/main/data/dest.csv')
 
-# df_countries = pd.read_csv(
-#     "https://raw.githubusercontent.com/albertyw/avenews/master/old/data/average-latitude-longitude-countries.csv"
-# )
 
 df_refugees = pd.read_csv(
     "https://raw.githubusercontent.com/purplesmurf45/Migration-visualisation/main/data/country_codes.csv"
 )
 
-# df_lat_lon = pd.read_csv(
-#     os.path.join(APP_PATH, os.path.join("data", "lat_lon_counties.csv"))
-# )
-# df_lat_lon["FIPS "] = df_lat_lon["FIPS "].apply(lambda x: str(x).zfill(5))
 
-# df_full_data = pd.read_csv(
-#     os.path.join(
-#         APP_PATH, os.path.join("data", "age_adjusted_death_rate_no_quotes.csv")
-#     )
-# )
-# df_full_data["County Code"] = df_full_data["County Code"].apply(
-#     lambda x: str(x).zfill(5)
-# )
-# df_full_data["County"] = (
-#     df_full_data["Unnamed: 0"] + ", " + df_full_data.County.map(str)
-# )
 
 YEARS = list(range(2000, 2017, 1))
 
-x= 294000
-beans = [
-    0,
-    x,
-    2*x,
-    3*x,
-    4*x,
-    5*x,
-    6*x,
-    7*x,
-    8*x,
-    9*x,
-    10*x,
-    11*x,
-    12*x,
-    13*x,
-    14*x
-]
-
-
-
-BINS = [
-    "0-2",
-    "2.1-4",
-    "4.1-6",
-    "6.1-8",
-    "8.1-10",
-    "10.1-12",
-    "12.1-14",
-    "14.1-16",
-    "16.1-18",
-    "18.1-20",
-    # "20.1-22",
-    # "22.1-24",
-    # "24.1-26",
-    # "26.1-28",
-    # "28.1-30",
-    # ">30",
-]
-
-DEFAULT_COLORSCALE = [
-    "#f2fffb",
-    "#bbffeb",
-    "#98ffe0",
-    "#79ffd6",
-    "#6df0c8",
-    "#69e7c0",
-    "#59dab2",
-    "#45d0a5",
-    "#31c194",
-    "#2bb489",
-    # "#25a27b",
-    # "#1e906d",
-    # "#188463",
-    # "#157658",
-    # "#11684d",
-    # "#10523e",
-]
 
 DEFAULT_OPACITY = 0.8
 
-mapbox_access_token = "pk.eyJ1IjoiYW5qYW52aWthcyIsImEiOiJjbDBwbWF0aHQyMWJwM2JuNTFweGk1NTc4In0.ymnQcTSbm2ieGEvIo6BNeA"
-mapbox_style = "mapbox://styles/plotlymapbox/cjvprkf3t1kns1cqjxuxmwixz"
 
 # App layout
 
@@ -128,14 +51,6 @@ app.layout = html.Div(
         html.Div(
             id="header",
             children=[
-                # html.A(
-                #     html.Img(id="logo", src=app.get_asset_url("dash-logo.png")),
-                #     href="https://plotly.com/dash/",
-                # ),
-                # html.A(
-                #     html.Button("Enterprise Demo", className="link-button"),
-                #     href="https://plotly.com/get-demo/",
-                # ),
                 html.A(
                     html.Button("Source Code", className="link-button"),
                     href="https://github.com/purplesmurf45/Migration-visualisation",
@@ -192,16 +107,6 @@ app.layout = html.Div(
                                     id="country-choropleth",
                                     figure=dict(
                                         layout=dict(
-                                            # mapbox=dict(
-                                            #     layers=[],
-                                            #     # accesstoken=mapbox_access_token,
-                                            #     # style=mapbox_style,
-                                            #     center=dict(
-                                            #         lat=38.72490, lon=-95.61446
-                                            #     ),
-                                            #     pitch=0,
-                                            #     zoom=5.5,
-                                            # ),
                                             autosize=True,
                                         ),
                                     ),
@@ -262,7 +167,9 @@ def display_map(year, figure):
     df_temp = df_refugees[df_refugees['Year'] == year]
     fig = go.Figure(data=go.Choropleth(
     locations = df_temp['CODE'],
-    z = df_temp['Refugees'],
+    z = np.log10(df_temp['Refugees']),
+    zmin = 0,
+    zmax = 6.6,
     text = df_temp['Destination'],
     colorscale = 'tealrose',
     autocolorscale=False,
@@ -270,7 +177,7 @@ def display_map(year, figure):
     marker_line_color="#2cfec1",
     marker_line_width=0.2,
     # colorbar_tickprefix = '$',
-    # colorbar_title = 'Number of Immigrants',
+    colorbar_title = 'Number of<br>Immigrants',
 ))
 
     fig.update_layout(
@@ -294,105 +201,32 @@ def display_map(year, figure):
             text="Number of <br>Immigrants",
             font=dict(color="#2cfec1"),
             bgcolor="#1f2630",
-            x=1,
-            y=1,
+            x=1.4,
+            y=1.4,
         )
     ]
     )
-
     fig.update_geos(bgcolor="rgba(0,0,0,0)")
-
+    fig_layout = fig["layout"]
+    fig_layout["paper_bgcolor"] = "#1f2630"
+    fig_layout["plot_bgcolor"] = "#1f2630"
+    fig_layout["font"]["color"] = "#2cfec1"
+    fig_layout["title"] = ""
+    fig_layout["title"]["font"]["color"] = "#2cfec1"
+    fig_layout["xaxis"]["tickfont"]["color"] = "#2cfec1"
+    fig_layout["yaxis"]["tickfont"]["color"] = "#2cfec1"
+    fig_layout["xaxis"]["gridcolor"] = "#5b5b5b"
+    fig_layout["yaxis"]["gridcolor"] = "#5b5b5b"
     return fig
 
-# def display_map(year, figure):
-#     cm = dict(zip(BINS, DEFAULT_COLORSCALE))
 
-#     data = [
-#         dict(
-#             lat=df_countries["Latitude"],
-#             lon=df_countries["Longitude"],
-#             text=df_countries["Country"],
-#             type="scattermapbox",
-#             hoverinfo="text",
-#             marker=dict(size=5, color="white", opacity=0),
-#         )
-#     ]
 
-#     annotations = [
-#         dict(
-#             showarrow=False,
-#             align="right",
-#             text="<b>Immigrants per country <br> per year</b>",
-#             font=dict(color="#2cfec1"),
-#             bgcolor="#1f2630",
-#             x=0.95,
-#             y=0.95,
-#         )
-#     ]
-
-#     # for i, bin in enumerate(reversed(BINS)):
-#     #     color = cm[bin]
-#     #     annotations.append(
-#     #         dict(
-#     #             arrowcolor=color,
-#     #             text=bin,
-#     #             x=0.95,
-#     #             y=0.85 - (i / 20),
-#     #             ax=-60,
-#     #             ay=0,
-#     #             arrowwidth=5,
-#     #             arrowhead=0,
-#     #             bgcolor="#1f2630",
-#     #             font=dict(color="#2cfec1"),
-#     #         )
-#     #     )
-
-#     if "layout" in figure:
-#         lat = figure["layout"]["mapbox"]["center"]["lat"]
-#         lon = figure["layout"]["mapbox"]["center"]["lon"]
-#         zoom = figure["layout"]["mapbox"]["zoom"]
-#     else:
-#         lat = 38.72490
-#         lon = -95.61446
-#         zoom = 3.5
-
-#     layout = dict(
-#         mapbox=dict(
-#             layers=[],
-#             accesstoken=mapbox_access_token,
-#             style=mapbox_style,
-#             center=dict(lat=lat, lon=lon),
-#             zoom=zoom,
-#         ),
-#         hovermode="closest",
-#         margin=dict(r=0, l=0, t=0, b=0),
-#         annotations=annotations,
-#         dragmode="lasso",
-#     )
-
-#     base_url = "https://raw.githubusercontent.com/jackparmer/mapbox-counties/master/"
-#     for bin in BINS:
-#         geo_layer = dict(
-#             sourcetype="geojson",
-#             source=base_url + str(year) + "/" + bin + ".geojson",
-#             type="fill",
-#             color=cm[bin],
-#             opacity=DEFAULT_OPACITY,
-#             # CHANGE THIS
-#             fill=dict(outlinecolor="#afafaf"),
-#         )
-#         layout["mapbox"]["layers"].append(geo_layer)
-
-#     fig = dict(data=data, layout=layout)
-#     return fig
-
-from sklearn import preprocessing
 le = preprocessing.LabelEncoder()
 a = np.unique(np.concatenate((df_od['Origin'],df_od['Destination']),0))
 le.fit(a)
 @app.callback(Output("heatmap-title", "children"), [Input("years-slider", "value")])
 def update_map_title(year):
-    return "Choropleths of immigrants per country \
+    return "Choropleth of immigrants per country \
 				in year {0}".format(
         year
     )
@@ -493,7 +327,7 @@ def display_selected_data(selectedData, chart_dropdown, year):
     dff = dff[dff["Destination"].isin(locations)]
 
     if chart_dropdown != "death_rate_all_time":
-        title = "Absolute deaths per county, <b>1999-2016</b>"
+        title = "Refugees in year <b>{0}</b>".format(year)
         AGGREGATE_BY = "Refugees"
         KIND = "bar"
         if "Bar Chart" == chart_dropdown:
@@ -566,30 +400,7 @@ def display_selected_data(selectedData, chart_dropdown, year):
             fig["layout"]["title"] = "Connectivity between countries in the year <b>{0}</b>".format(year)
 
 
-            
-
-
-        # fig.update_layout(title_text="Basic Sankey Diagram", font_size=10)
-        # fig.show()
-
-        # dff[AGGREGATE_BY] = pd.to_numeric(dff[AGGREGATE_BY], errors="coerce")
-        # deaths_or_rate_by_fips = dff.groupby("Destination")[AGGREGATE_BY].sum()
-        # deaths_or_rate_by_fips = deaths_or_rate_by_fips.sort_values()
-
-        # # Only look at non-zero rows:
-        # deaths_or_rate_by_fips = deaths_or_rate_by_fips[deaths_or_rate_by_fips > 0]
-        # fig = deaths_or_rate_by_fips.iplot(
-        #     kind=KIND, y=AGGREGATE_BY, title=title, asFigure=True
-        # )
-
         fig_layout = fig["layout"]
-        # fig_data = fig["data"]
-
-        # fig_data[0]["text"] = deaths_or_rate_by_fips.values.tolist()
-        # fig_data[0]["marker"]["color"] = "#2cfec1"
-        # fig_data[0]["marker"]["opacity"] = 1
-        # fig_data[0]["marker"]["line"]["width"] = 0
-        # fig_data[0]["textposition"] = "outside"
         fig_layout["paper_bgcolor"] = "#1f2630"
         fig_layout["plot_bgcolor"] = "#1f2630"
         fig_layout["font"]["color"] = "#2cfec1"
@@ -604,64 +415,6 @@ def display_selected_data(selectedData, chart_dropdown, year):
         fig_layout["margin"]["l"] = 50
 
         return fig
-
-    # fig = dff.iplot(
-    #     kind="area",
-    #     x="Year",
-    #     y="Age Adjusted Rate",
-    #     text="County",
-    #     categories="County",
-    #     colors=[
-    #         "#1b9e77",
-    #         "#d95f02",
-    #         "#7570b3",
-    #         "#e7298a",
-    #         "#66a61e",
-    #         "#e6ab02",
-    #         "#a6761d",
-    #         "#666666",
-    #         "#1b9e77",
-    #     ],
-    #     vline=[year],
-    #     asFigure=True,
-    # )
-
-    # for i, trace in enumerate(fig["data"]):
-    #     trace["mode"] = "lines+markers"
-    #     trace["marker"]["size"] = 4
-    #     trace["marker"]["line"]["width"] = 1
-    #     trace["type"] = "scatter"
-    #     for prop in trace:
-    #         fig["data"][i][prop] = trace[prop]
-
-    # # Only show first 500 lines
-    # fig["data"] = fig["data"][0:500]
-
-    # fig_layout = fig["layout"]
-
-    # # See plot.ly/python/reference
-    # fig_layout["yaxis"]["title"] = "Age-adjusted death rate per county per year"
-    # fig_layout["xaxis"]["title"] = ""
-    # fig_layout["yaxis"]["fixedrange"] = True
-    # fig_layout["xaxis"]["fixedrange"] = False
-    # fig_layout["hovermode"] = "closest"
-    # fig_layout["title"] = "<b>{0}</b> counties selected".format(len(fips))
-    # fig_layout["legend"] = dict(orientation="v")
-    # fig_layout["autosize"] = True
-    # fig_layout["paper_bgcolor"] = "#1f2630"
-    # fig_layout["plot_bgcolor"] = "#1f2630"
-    # fig_layout["font"]["color"] = "#2cfec1"
-    # fig_layout["xaxis"]["tickfont"]["color"] = "#2cfec1"
-    # fig_layout["yaxis"]["tickfont"]["color"] = "#2cfec1"
-    # fig_layout["xaxis"]["gridcolor"] = "#5b5b5b"
-    # fig_layout["yaxis"]["gridcolor"] = "#5b5b5b"
-
-    # if len(fips) > 500:
-    #     fig["layout"][
-    #         "title"
-    #     ] = "Age-adjusted death rate per county per year <br>(only 1st 500 shown)"
-
-    # return fig
 
 
 if __name__ == "__main__":
